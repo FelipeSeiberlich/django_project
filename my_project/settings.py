bcc03ee854e0c7bb6c72150f76c91cfc9cdbe61c
Felipe Seiberlich <philzaiber@outlook.com>
2023-12-20 10:55:11 +0000

    Creat a new Secret Key and add to config vars at Heroku

--- my_project/settings.py
+++ my_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sozl%lgeajzyzt!fq%fgo4_1hm@9(jc&rtt$mr8r5p@#ysdcz2'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
